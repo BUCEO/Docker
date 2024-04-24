95c351d7488f765d85dbef42cd1b5e19dd3bbadd
Juan Damián Pajares <damian.pajares@docente.ceibal.edu.uy>
2024-04-24 12:52:57 -0300

    Agrego hola mundo en php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        
+        <?php 
+        phpinfo( );
+
+        echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
