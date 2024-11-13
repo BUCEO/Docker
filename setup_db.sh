746a0920467e58e16be0a6c65175e190122ae212
damianpajares <damian.pajares@docente.ceibal.edu.uy>
2024-11-13 11:41:45 -0300

    cambios en master slave

--- setup_db.sh
+++ setup_db.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-sql_slave_user='CREAR USUARIO "mydb_slave_user"@"%" IDENTIFICADO POR "mydb_slave_pwd"; CONCEDER ESCLAVO DE REPLICACIÓN EN *.* A "mydb_slave_user"@"%"; VACIAR PRIVILEGIOS;' 
+export $(grep -v '^#' .env | xargs)
+
+sql_slave_user='CREATE USER "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
 docker exec database_master sh -c "mysql -u root -pS3cret -e '$sql_slave_user'"
-MS_STATUS=`docker exec database_master sh -c 'mysql -u root -pS3cret -e "MOSTRAR ESTADO DEL MAESTRO"'` 
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'` 
+MS_STATUS=`docker exec database_master sh -c 'mysql -u root -pS3cret -e "SHOW MASTER STATUS"'`
+CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
-sql_set_master="CAMBIAR MAESTRO A HOST_MAESTRO='database_master', USUARIO_MAESTRO='mydb_slave_user', CONTRASEÑA_MAESTRO='mydb_slave_pwd', ARCHIVO_REGISTRO_MAESTRO='$REGISTRO_ACTUAL', POS_REGISTRO_MAESTRO=$POS_ACTUAL; INICIAR ESCLAVO;" 
-start_slave_cmd='mysql -u root -pS3cret -e "' 
-start_slave_cmd+="$sql_set_master" 
-start_slave_cmd+='"' 
+sql_set_master="CHANGE MASTER TO MASTER_HOST='database_master',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+start_slave_cmd='mysql -u root -pS3cret -e "'
+start_slave_cmd+="$sql_set_master"
+start_slave_cmd+='"'
 docker exec database_slave sh -c "$start_slave_cmd"
-docker exec database_slave sh -c "mysql -u root -pS3cret -e 'MOSTRAR ESTADO DEL ESCLAVO \G'"
+docker exec database_slave sh -c "mysql -u root -pS3cret -e 'SHOW SLAVE STATUS \G'"
